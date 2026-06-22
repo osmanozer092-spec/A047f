4215f34e16970b74e0aaca37410e569b0aa6b1bb
osmanozer092-spec <osmanozer092@gmail.com>
2026-06-22 20:04:24 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,78 @@
 #!/bin/bash
 
+export RDIR="$(pwd)"
 export ARCH=arm64
+export KBUILD_BUILD_USER="@ravindu644"
 export PLATFORM_VERSION=12
 export ANDROID_MAJOR_VERSION=s
 
-make ARCH=arm64 exynos850-a04sxx_defconfig
-make ARCH=arm64 -j16
+# Install requirements
+if [ ! -f ".requirements" ]; then
+    sudo apt update && sudo apt install -y git device-tree-compiler lz4 xz-utils zlib1g-dev openjdk-17-jdk gcc g++ python3 python-is-python3 p7zip-full android-sdk-libsparse-utils erofs-utils \
+        default-jdk git gnupg flex bison gperf build-essential zip curl libc6-dev libncurses-dev libx11-dev libreadline-dev libgl1 libgl1-mesa-dev \
+        python3 make sudo gcc g++ bc grep tofrodos python3-markdown libxml2-utils xsltproc zlib1g-dev python-is-python3 libc6-dev libtinfo6 \
+        make repo cpio kmod openssl libelf-dev pahole libssl-dev --fix-missing && wget http://security.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.3-2ubuntu0.1_amd64.deb && sudo dpkg -i libtinfo5_6.3-2ubuntu0.1_amd64.deb && touch .requirements
+fi
+
+export BUILD_CROSS_COMPILE="${RDIR}/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+export BUILD_CC="${RDIR}/toolchain/clang/host/linux-x86/clang-r353983c/bin/clang"
+
+#init ksu next
+git submodule init && git submodule update
+
+#output dir
+if [ ! -d "${RDIR}/out" ]; then
+    mkdir -p "${RDIR}/out"
+fi
+
+#build dir
+if [ ! -d "${RDIR}/build" ]; then
+    mkdir -p "${RDIR}/build"
+else
+    rm -rf "${RDIR}/build" && mkdir -p "${RDIR}/build"
+fi
+
+#kernelversion
+if [ -z "$BUILD_KERNEL_VERSION" ]; then
+    export BUILD_KERNEL_VERSION="dev"
+fi
+
+#setting up localversion
+echo -e "CONFIG_LOCALVERSION_AUTO=n\nCONFIG_LOCALVERSION=\"-ravindu644-${BUILD_KERNEL_VERSION}\"\n" > "${RDIR}/arch/arm64/configs/version.config"
+
+#build options
+export ARGS="
+-w \
+-C $(pwd) \
+O=$(pwd)/out \
+-j$(nproc) \
+ARCH=arm64 \
+CROSS_COMPILE=${BUILD_CROSS_COMPILE} \
+CC=${BUILD_CC} \
+"
+
+#build kernel image
+build_kernel(){
+    make ${ARGS} exynos850-a04sxx_defconfig a04s.config version.config
+    make ${ARGS} menuconfig
+    make ${ARGS} || exit 1
+}
+
+#build boot.img
+build_boot() {    
+    rm -f ${RDIR}/AIK-Linux/split_img/boot.img-kernel ${RDIR}/AIK-Linux/boot.img
+    cp "${RDIR}/out/arch/arm64/boot/Image" ${RDIR}/AIK-Linux/split_img/boot.img-kernel
+    mkdir -p ${RDIR}/AIK-Linux/ramdisk/{debug_ramdisk,dev,metadata,mnt,proc,second_stage_resources,sys}
+    cd ${RDIR}/AIK-Linux && ./repackimg.sh --nosudo && mv image-new.img ${RDIR}/build/boot.img
+}
+
+#build odin flashable tar
+build_tar(){
+    cd ${RDIR}/build
+    tar -cvf "KernelSU-Next-SM-A047F-${BUILD_KERNEL_VERSION}.tar" boot.img && rm boot.img
+    echo -e "\n[i] Build Finished..!\n" && cd ${RDIR}
+}
+
+build_kernel
+build_boot
+build_tar
